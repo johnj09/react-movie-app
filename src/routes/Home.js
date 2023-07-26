9c1580b8834ccfe08ac427b3261fa1c81830f929
johnj09 <johnjangjaeho123@gmail.com>
2023-07-26 15:34:42 +0800

    Add css, changed minor issues

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import {useState, useEffect} from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home() {
 	const [loading, setLoading] = useState(true);
@@ -7,7 +8,7 @@ function Home() {
 	const getMovies = async () => {
 		const json = await (
 			await fetch(
-				`https://yts.mx/api/v2/list_movies.json?minimum_rating=9.5&sort_by=year`
+				`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.0&sort_by=year`
 			)
 		).json();
 		setMovies(json.data.movies);
@@ -17,17 +18,20 @@ function Home() {
 		getMovies();
 	}, []);
 	return (
-		<div>
+		<div className={styles.container}>
 			{loading ? (
-				<h1>Loading...</h1>
+				<div className={styles.loader}>
+					<h1>Loading...</h1>
+				</div>
 			) : (
-				<div>
+				<div className={styles.movies}>
 					{movies.map((movie) => (
 						<Movie
 							key={movie.id}
 							id={movie.id}
 							coverImg={movie.medium_cover_image}
 							title={movie.title}
+							year={movie.year}
 							summary={movie.summary}
 							genres={movie.genres}
 						/>
